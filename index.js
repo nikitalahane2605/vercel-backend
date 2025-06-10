4d8836f61c0820e8d7a2fe940335923e0fd9a6c2
nikitalahane2605 <9325671959@myscoe.in>
2025-06-10 12:09:06 +0530

    fix: chnag multer strorage

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
-const upload = multer({ storage: multer.memoryStorage() });
 const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
@@ -25,7 +24,7 @@ const storage = multer.diskStorage({
     return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
   }
 })
-
+const upload = multer({ storage: storage })
 app.post("/upload", upload.single('product'), (req, res) => {
   res.json({
     success: 1,
